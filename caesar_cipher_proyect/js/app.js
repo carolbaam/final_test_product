9cab2cbedeee98dd2567fee4f308f815143a511c
carolbaam <carolinabaam@gmail.com>
2017-10-30 16:34:23 -0600

    actualizando js de caesar cipher

--- caesar_cipher_proyect/js/app.js
+++ caesar_cipher_proyect/js/app.js
@@ -1,19 +1,15 @@
 var text=prompt('ingresa tu frase');
 var toUpperText= text.toUpperCase();
+console.log(toUpperText);
 
 //if(toUpperCase/^d/.test || toUpperCase /^s/.test ){
-
-for (var i =0; var i<toUpperText.length; i++) {
-  if ((i)==="" || (i)===Number) {
-    alert('no debes dejar espacios ni ingresar numeros');
-  }else{
-    return true
-  }
+/*
+while(toUpperText ==='' || toUpperText!==='String' ){
+  text=prompt('ingresa tu frase');
 }
 
-
-
-
+console.log()
+*/
 var cipher= function(toUpperText){
   var cipherText="";
   for (var i=0; i<toUpperText.length; i++){
@@ -26,3 +22,29 @@ var cipher= function(toUpperText){
   return cipherText;
   
 }
+
+console.log(cipher(toUpperText));
+
+var decipher= function(toUpperText){
+  var decipherText="";
+  for (var i=0; i<toUpperText.length; i++){
+    
+    var textChar=(toUpperText.charCodeAt(i)+26)%26+65;
+    
+    decipherText+= String.fromCharCode(textChar);
+ 
+  }
+
+  
+  return decipherText;
+  
+}
+console.log(decipher(toUpperText));
+
+
+//-65-33)%26+65;
+
+
+
+
+
