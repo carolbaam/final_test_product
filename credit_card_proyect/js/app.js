9cdc336be9190110049f648b1f7b35cade4a99bd
carolbaam <carolinabaam@gmail.com>
2017-10-30 18:57:13 -0600

    actualizando con comentarios

--- credit_card_proyect/js/app.js
+++ credit_card_proyect/js/app.js
@@ -1,51 +1,49 @@
 
-var isValidCard= function(array){
-    
-  var evenPosition=[];
-  var creditCardNum=prompt('Ingrese número de tarjeta');
+
+   //creando prompt que pide número de tarjeta
+   var creditCardNum=prompt('Ingrese número de tarjeta');
   
-  var isValidCard=function (creditCardNum) {
-  
-  while(creditCardNum==="" || isNaN(parseInt(creditCardNum))) {
+     var isValidCard=function (creditCardNum) {
+  //filtro con while para evitar que pase un campo vacío y un elemento no numérico, si no se cumple, tevolver prompt
+         while(creditCardNum==="" || isNaN(parseInt(creditCardNum))) {
     
-    creditCardNum=prompt('Ingresa números de tu tarjeta');
+         creditCardNum=prompt('Ingresa números de tu tarjeta');
     
-  }
+         }
   //convirtiendo a array y volteando
-  var toArrayReverseNum=creditCardNum.split("").reverse();
-  console.log(toArrayReverseNum);
+    var toArrayReverseNum=creditCardNum.split("").reverse();
+    console.log(toArrayReverseNum);
   
-  
-  for (var i=0; i<toArrayReverseNum.length; i++){
-      var evenOperation=[];
-      var sum=[];
-      if((i+1)%2===0){
-        evenOperation=toArrayReverseNum[i]*2;
-        toArrayReverseNum[i]=evenOperation
-      if (evenOperation>=10){
-      evenOperation=parseInt((evenOperation/10)+(evenOperation%10));
-        toArrayReverseNum[i]=evenOperation;
-        
-      }
+  //iternado sobre array para encontrar indice par, y si es par y mayor a 10 sumar resultado de división y modulo de 10
+    for (var i=0; i<toArrayReverseNum.length; i++){
+        var evenOperation=[];
+        var sum=[];
+            if((i+1)%2===0){
+               evenOperation=toArrayReverseNum[i]*2;
+               toArrayReverseNum[i]=evenOperation
+           if (evenOperation>=10){
+               evenOperation=parseInt((evenOperation/10)+(evenOperation%10));
+               toArrayReverseNum[i]=evenOperation;
+           
+           }
       
-    }
-       else{
+              }   else {
          toArrayReverseNum[i]=parseInt(toArrayReverseNum[i]);
-       }
-       sum=sum + parseInt(toArrayReverseNum[i]);
+             }//sumar elementos en indices y no índices 
+                 sum=sum + parseInt(toArrayReverseNum[i]);
     
-  }
+             }
   
-  
-  var moduloOp = sum%10;
-  if (moduloOp===0){
-    return "Tarjeta válida";
-  }
-  else{
-   return "Tarjeta inválida ";
-  }
-  }
-  console.log(isValidCard(creditCardNum));
+  //realizar operación modulo10 a la suma total para saber si es valida o no
+         var moduloOp = sum%10;
+              if (moduloOp===0){
+                 return "Tarjeta válida";
+              } else {
+                  return "Tarjeta inválida ";
+             }
+             }
+          
+             console.log(isValidCard(creditCardNum));
   
   
   
